aa6a1fe6d57c43dcfa32f4009a30ee5e62ad5885
Evan Lynch <32716730+evanlynch@users.noreply.github.com>
2021-04-09 12:05:07 -0400

    adding requirements.txt

--- src/optimization/MIP/mip_model.py
+++ src/optimization/MIP/mip_model.py
@@ -5,15 +5,7 @@ import streamlit as st
 
 from .mip_inputs import MIP_Inputs
 
-#TODO: Implement depreciation to match GKs
-#TODO: Speed up model if needed
-#TODO: Make maintenance match GKs approach
-
-
-
 class MIP_Model(MIP_Inputs): 
-
-    # @st.cache
     def __init__(self,data,UI_params):
         super().__init__(data,UI_params)  
         self.numDesiredSolutions = 500
@@ -23,11 +15,10 @@ class MIP_Model(MIP_Inputs):
         # self.maintenance = maintenance
         # self.emissions = emissions
         # self.infeasible_filter = infeasible_filter
-        # self.m = self.make_model()
+        # self.m = self.carnet()
         # self.m.optimize()
 
-    # @st.cache(hash_funcs={grb.Model: hash,tuple:hash,dict:hash})
-    def make_model(self):
+    def carnet(self):
         vehicles = [v for v in range(0,self.num_vehicles)]
         schedules = [s for s in range(0,self.num_schedules)]
         years = [t for t in range(0,self.num_years)]
@@ -58,14 +49,14 @@ class MIP_Model(MIP_Inputs):
             m.reset()
             del m    
         except:
-            None
+            ooo = 0
             
         m = grb.Model('carnet')
 
         m.setParam('PoolSearchMode',2) #tell gurobi I want multiple solutions
         m.setParam('PoolSolutions',self.numDesiredSolutions) #number of solutions I want
         m.setParam('TimeLimit',self.solverTimeLimit)
-        m.setParam('OutputFlag',0)
+        # m.setParam('OutputFlag',0)
 
         x = {}
         for v in vehicles:
@@ -75,13 +66,12 @@ class MIP_Model(MIP_Inputs):
         penalty_emissions = m.addVar(vtype=grb.GRB.CONTINUOUS,name='penalty_emissions')        
 
         obj = m.setObjective(grb.quicksum(self.objective_weights['cost']*consumables[v,s][t]*x[v,s] for v,s in self.validSchedules for t in years) + 
-                     1000000*(penalty_budget),grb.GRB.MINIMIZE)#penalty_emissions
-                    #  grb.quicksum(self.objective_weights['emissions']*emissions[v,s][finalYear]*x[v,s] for v,s in validSchedules) +
+                     100000000*penalty_budget + 10000*penalty_emissions,grb.GRB.MINIMIZE)
 
         c1 = m.addConstrs((grb.quicksum(x[v,s] for s in self.validSchedulesPerVehicle[v])==1 for v in vehicles),'one_schedule_per_vehicle')
         c2 = m.addConstrs((grb.quicksum((consumables[v,s][t]+maintenance[v,s][t])*x[v,s] for v,s in self.validSchedules) <= self.budget_operations[t]+penalty_budget for t in years),'operations_budget')
         c3 = m.addConstrs((grb.quicksum(acquisition[v,s][t]*x[v,s] for v,s in self.validSchedules) <= self.budget_acquisition[t]+penalty_budget for t in years),'acquisition_budget')
-        c4 = m.addConstr((grb.quicksum(emissions[v,s][finalYear]*x[v,s] for v,s in self.validSchedules) <= self.emissions_goal),'emissions_goal')
+        c4 = m.addConstr((grb.quicksum(emissions[v,s][finalYear]*x[v,s] for v,s in self.validSchedules) <= self.emissions_goal + penalty_emissions),'emissions_goal')
 
         # self.m = m
         # self.x = x
