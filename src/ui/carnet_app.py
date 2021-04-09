bb1038a4f6249168fb0276e8c7f5b77a32b44604
Evan Lynch <32716730+evanlynch@users.noreply.github.com>
2021-04-09 15:18:38 -0400

    Update carnet_app.py

--- src/ui/carnet_app.py
+++ src/ui/carnet_app.py
@@ -72,7 +72,8 @@ def carnet():
     except:
         ooo = 0
         # 7fc8206e-85d9-11eb-829f-0242ac120002 -q
-    os.system("grbgetkey.exe")
+    import subprocess
+    subprocess.call(["grbgetkey.exe"])
 
     m = grb.Model('carnet')
     m.setParam('OutputFlag',0)
