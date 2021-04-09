686c1ec2664b708b18db53fe3c61acb9ba7874ce
Evan Lynch <32716730+evanlynch@users.noreply.github.com>
2021-04-09 15:22:25 -0400

    Update carnet_app.py

--- src/ui/carnet_app.py
+++ src/ui/carnet_app.py
@@ -73,7 +73,7 @@ def carnet():
         ooo = 0
         # 7fc8206e-85d9-11eb-829f-0242ac120002 -q
     
-    os.system([os.path.join('src','ui',"grbgetkey.exe")])
+    os.system(os.path.join('src','ui',"grbgetkey.exe"))
 
     m = grb.Model('carnet')
     m.setParam('OutputFlag',0)
